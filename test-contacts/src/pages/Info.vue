8250ee817dde9b395ba8afed91ef0f5db0019460
nastyakpi1995 <nastyakpi1995@gmail.com>
2020-10-10 13:54:03 +0300

    add info style

--- test-contacts/src/pages/Info.vue
+++ test-contacts/src/pages/Info.vue
@@ -2,7 +2,48 @@
 	<div class="info">
 		<div class="info_form_group">
 			<div class="info_fm_fields">
-				
+				<h2 class="info_title_name">Annie Bing</h2>
+
+				<ul class="info_list">
+					<li class="info_item">
+						<div class="info_text_block">
+							<img class="info_icon_tab" src="@/assets/keyboard_return-24px.svg">
+							<span class="info_name">address:</span>
+							<span class="info_value">Макарова 6 кв 55</span>
+						</div>
+
+						<div class="info_icons_block">
+							<img class="info_icon" src="@/assets/create-24px.svg" alt="">
+							<img class="info_icon" src="@/assets/delete_forever-24px.svg" alt="">
+						</div>
+					</li>
+
+					<li class="info_item">
+						<div class="info_text_block">
+							<img class="info_icon_tab" src="@/assets/keyboard_return-24px.svg">
+							<span class="info_name">tel:</span>
+							<span class="info_value">+380 (067) 6789874</span>
+						</div>
+
+						<div class="info_icons_block">
+							<img class="info_icon" src="@/assets/create-24px.svg" alt="">
+							<img class="info_icon" src="@/assets/delete_forever-24px.svg" alt="">
+						</div>
+					</li>
+					<li class="info_item">
+						<div class="info_text_block">
+							<img class="info_icon_tab" src="@/assets/keyboard_return-24px.svg">
+							<span class="info_name">email:</span>
+							<span class="info_value">test@gmail.com</span>
+						</div>
+
+						<div class="info_icons_block">
+							<img class="info_icon" src="@/assets/create-24px.svg" alt="">
+							<img class="info_icon" src="@/assets/delete_forever-24px.svg" alt="">
+						</div>
+					</li>
+				</ul>
+
 			</div>
 			<Button text="Add Field" />
 		</div>
@@ -24,6 +65,44 @@
 
 	.info {
 		padding: 20px;
-		
+
+		.info_title_name {
+			color: $light;
+		}
+		.info_list {
+			border-bottom: 1px solid $grey;
+			margin-bottom: 30px;
+
+			.info_item {
+				padding: 10px 0 10px 40px;
+				display: flex;
+				justify-content: space-between;
+				color: $light;
+
+				.info_text_block {
+					display: flex;
+					align-items: center;
+				}
+
+				.info_icon_tab {
+					filter: invert(60%);
+					transform: rotateY(180deg);
+					color: $grey;
+					cursor: pointer;
+				}
+
+				.info_name {
+					padding: 0 10px;
+				}
+
+				.info_icon {
+					filter: invert(1);
+					cursor: pointer;
+				}
+				.info_icon:first-child {
+					margin-right: 20px;
+				}
+			}
+		}
 	}
 </style>
